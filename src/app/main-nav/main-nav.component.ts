779f8651301af71bde38830b71282127668ef885
Bryan Lewis <bryananthonylewis@gmail.com>
2018-06-21 15:10:37 -0700

    Initial commit

---
+++ src/app/main-nav/main-nav.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "app-main-nav",
+  templateUrl: "./main-nav.component.html",
+  styleUrls: ["./main-nav.component.scss"]
+})
+export class MainNavComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
